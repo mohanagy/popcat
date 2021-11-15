4868ad44c4945c95ab59af935ba3ce17c023fa86
Mohammed Naji <moha.buy@gmail.com>
2021-11-15 19:43:15 +0400

    Fix

---
+++ src/index.ts
@@ -0,0 +1,47 @@
+#!/usr/bin/env node
+
+import axios from "axios";
+import fs from "fs";
+import path from "path";
+
+
+const file1 = await fs.readFileSync(path.resolve('./', 'directory_only_one.small.txt'), 'utf8')
+const file1Words = file1.split('/\n')
+const file2 = await fs.readFileSync(path.resolve('./', 'Subdomain.txt'), 'utf8')
+const file2Words = file2.split('/\n')
+const wait = (time: number) => new Promise((resolve) => {
+
+    setTimeout(resolve, time);
+})
+
+const doit = async (url: string, words: string[], subdomain: boolean, name: string) => {
+
+    for await (const word of words) {
+        console.log(`process with name :${name} and url :${url} and subdomain is ${subdomain ? 'ON' : 'OFF'} `)
+        try {
+            const response = await axios.get(`${subdomain && 'https://' + word + '.'}${url}${!subdomain && word} `);
+            await wait(3000)
+            console.log({
+                subdomain,
+                url,
+                name,
+                word: response.status
+
+            })
+
+        } catch (error: any) {
+
+        }
+    }
+}
+
+await doit('https://www.umnea.net/', file1Words, false, 'list1')
+await doit('https://www.umnea.net/', file2Words, false, 'list2')
+await doit('umnea.net/', file1Words, true, 'subdomainList1')
+await doit('umnea.net/', file2Words, true, 'subdomainLst2')
+
+
+setInterval(async () => {
+
+    await axios.get('/')
+}, 30000)
