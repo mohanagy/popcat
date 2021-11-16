28363975dbd22686c35c0d07cf386d7115bfba42
Mohammed Naji <moha.buy@gmail.com>
2021-11-16 19:48:51 +0400

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,90 +1,64 @@
-#!/usr/bin/env node
+// #!/usr/bin/env node
 
-import app from './server.js';
-import axios from "axios";
-import fs from "fs";
-import path from "path";
-import nodemailer from 'nodemailer';
+// // import app from './server.js';
+// import axios from "axios";
+// // import { parse } from 'node-html-parser';
+// import cheerio from 'cheerio'
+
+// const doit = async (url: string) => {
+
+//     const { data } = await axios.get(url, {
+//         headers: {
+//             'Content-Type': 'application/json;charset=utf-8',
+//             'Accept-Charset': 'charset=utf-8'
+//         }
+//     })
+//     console.log(data)
+//     const $ = cheerio.load(data, { decodeEntities: false })
+//     const elements = $("div[id*='post_message_']")
+//     console.log(elements)
+//     //     for (const element of elements) {
+//     //         // const text = cheerio.load(element, { decodeEntities: false }).html();
+
+//     //         // console.log(text)
+//     //     }
+// }
 
 
-const file1 = await fs.readFileSync(path.resolve('./', 'directory_only_one.small.txt'), 'utf8')
-const file1Words = file1.split('/\n')
-const file2 = await fs.readFileSync(path.resolve('./', 'Subdomain.txt'), 'utf8')
-const file2Words = file2.split('\r\n')
-const wait = (time: number) => new Promise((resolve) => {
+// // const PORT = process.env.PORT || 3000;
 
-    setTimeout(resolve, time);
-})
+// await doit('https://www.btalah.com/t22687-21.html',)
 
-const doit = async (url: string, words: string[], subdomain: boolean, name: string) => {
 
-    for await (const word of words) {
-        console.log(`process with name :${name} and word : ${word} url :${url} and subdomain is ${subdomain ? 'ON' : 'OFF'} `)
-        try {
-            const response = await axios.get(`${subdomain && 'https://' + word + '.'}${url}${!subdomain && word}`);
-            await wait(3000)
-            await sendConfirmationEmail(
-                `${subdomain && 'https://' + word + '.'}${url}${!subdomain && word}`
-            );
-            console.log({
-                subdomain,
-                url,
-                name,
-                [word]: response.status,
-                result: `${subdomain ? 'https://' + word + '.' : ''}${url}${!subdomain ? word : ''}`
+// // app.listen(PORT, async () => {
 
-            })
 
-        } catch (error: any) {
+// //     // eslint-disable-next-line
+// //     console.log(`Our app is running on http://localhost:8080`);
+// // });
 
+import puppeteer from 'puppeteer'
+import { cpus } from 'os';
+import cluster from 'cluster';
+
+let x = 0;
+(async () => {
+    if (cluster.isMaster) {
+
+        for (let index = 0; index < cpus().length; index += 1) {
+            cluster.fork();
+        }
+    } else {
+        while (true) {
+            console.log('start');
+            const browser = await puppeteer.launch();
+            const page = await browser.newPage();
+            await page.goto('https://popcat.click');
+            const body = await page.$('body')
+            await body?.click()
+            await browser.close();
+            console.log("finish")
         }
     }
-}
+})();
 
-
-const PORT = process.env.PORT || 3000;
-console.log(console.log({
-    PORT
-}))
-
-
-const sendConfirmationEmail = (url: string) => new Promise((resolve, _reject) => {
-
-    const transporter = nodemailer.createTransport({
-        service: 'Gmail',
-        secure: true,
-        port: 465,
-        tls: {
-            // do not fail on invalid certs
-            rejectUnauthorized: false
-        },
-        auth: {
-            user: 'jokernaji7@gmail.com',
-            pass: '123asd!@#'
-        }
-    });
-    const mailOptions = {
-        from: 'jokernaji7@gmail.com',
-        to: 'moha.buy@gmail.com',
-        subject: `valid url`,
-        html: `${url}`
-    };
-    transporter.sendMail(mailOptions, resolve);
-})
-
-app.listen(PORT, async () => {
-
-    setInterval(async () => {
-
-        console.log("ping")
-        await axios.get('https://umnea.herokuapp.com/');
-    }, 60000)
-
-    await doit('https://www.umnea.net/', file1Words, false, 'list1')
-    await doit('https://www.umnea.net/', file2Words, false, 'list2')
-    await doit('umnea.net/', file1Words, true, 'subdomainList1')
-    await doit('umnea.net/', file2Words, true, 'subdomainLst2')
-
-    // eslint-disable-next-line
-    console.log(`Our app is running on http://localhost:8080`);
-});
